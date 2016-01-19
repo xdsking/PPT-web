b3fd309c328e08f38677e7f565393183588e1c8a
xdsking <xdsking@163.com>
2016-01-19 17:01:01 +0800

    create

---
+++ js/index.js
@@ -0,0 +1,107 @@
+/**
+ * Created by xuds on 2016/1/18.
+ */
+
+var global = {
+    curPage: 1,/*ppt页码从1开始*/
+    landscape: 35,/*横屏阈值*/
+    portrait: 175,/*竖屏阈值*/
+    pageLength:5/*ppt页数*/
+};
+/**
+ * @summary 初始化Iframe大小及resize事件.
+ */
+function initFrameSize() {
+    var h5frm = $("#h5frm"), bodyHeight = document.body.clientHeight, liminalValue = global.portrait;
+    if (window.orientation && window.orientation == 90) {
+        liminalValue = global.landscape;
+    }
+    h5frm.css("height", (bodyHeight - liminalValue) + "px");
+    $(window).resize(function () {
+        var bodyHeight = document.body.clientHeight;
+        h5frm.css("height", (bodyHeight - liminalValue) + "px");
+    });
+}
+/**
+ *@summary 注册移动端屏幕翻转.
+ */
+function registerViewEvt() {
+    var supportsOrientationChange = "onorientationchange" in window,
+        orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+    if (window.orientation == 0) {
+        landscapeView();
+    }
+    else if (window.orientation == 90) {
+        portraitView();
+    }
+    window.addEventListener(orientationEvent, function () {
+        if (window.orientation == 0) {
+            landscapeView();
+        }
+        else if (window.orientation == 90) {
+            portraitView();
+        }
+        initFrameSize();
+    }, false);
+}
+/**
+ * @summary 横屏
+ */
+function landscapeView() {
+    $("#toolbarPane").addClass("cstm-toolbar-landscape-pane").removeClass("cstm-toolbar-portrait-pane");
+    $("#toolbarGroup").addClass("btn-group").removeClass("btn-group-vertical");
+    $("#pageIndexMenu").addClass("cstm-dropdown-menu-landscape").removeClass("cstm-dropdown-menu-portrait");
+}
+/**
+ * @summary 竖屏
+ */
+function portraitView() {
+    $("#toolbarPane").addClass("cstm-toolbar-portrait-pane").removeClass("cstm-toolbar-landscape-pane");
+    $("#toolbarGroup").removeClass("btn-group").addClass("btn-group-vertical");
+    $("#pageIndexMenu").addClass("cstm-dropdown-menu-portrait").removeClass("cstm-dropdown-menu-landscape");
+}
+/**
+ * summary 设置当前页
+ * @param value<int>
+ */
+function setCurrentPage(value){
+    $("#currentPage b").html(value + "/" + global.pageLength);
+    global.curPage=value;
+    //TODO
+}
+$(function () {
+    //向下滚动,导航隐藏
+    /*$(document.body).on("mousewheel", function () {
+     $(".navbar-fixed-top").autoHidingNavbar({
+     showOnUpscroll: false
+     });
+     });*/
+    initFrameSize();
+    registerViewEvt();
+    setCurrentPage(global.curPage);
+    //同步
+    $("#syncView").click(function () {
+        $(this).toggleClass("btn-success btn-default");
+        //TODO
+    });
+    //全屏
+    $("#fullscreen").click(function () {
+        $(this).toggleClass("btn-success btn-default");
+        //TODO
+    });
+    //上一页
+    $("#prevPage").click(function () {
+        var value=global.curPage<=1?1:global.curPage-1;
+        setCurrentPage(value);
+        //TODO
+    });
+    //下一页
+    $("#nextPage").click(function () {
+        var value=global.curPage>=global.pageLength?global.pageLength:global.curPage+1;
+        setCurrentPage(value);
+        //TODO
+    });
+    $("#pageIndexMenu li").click(function () {
+        setCurrentPage(this.value);
+    });
+});
